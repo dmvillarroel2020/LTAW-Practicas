7c5c2b38dfaae35b622b9891c0d0f0828e3d706d
dmvillarroel2020 <dm.villarroel.2020@alumnos.urjc.es>
2024-02-19 12:20:52 +0100

    asincronia

--- P1/S2.js
+++ P1/S2.js
@@ -191,18 +191,60 @@ function suma(x,y) {
   }
   
   //-- Llamar a suma
-  let a = suma(2,3);
+  let c = suma(2,3);
   
   //-- Probando la funcione mensaje
   mensaje("Prueba")
-  mensaje(a);
+  mensaje(c);
   
   //-- Probando la funcion call
   call(saluda);
   
   //-- Se le pasa como parametro una funcion
-  //-- que se define dentro de los parmatros, vez de 
+  //-- que se define dentro de los parametros, vez de 
   //-- fuera
   call( () => {
     mensaje("HOLI!!")
   });
+
+
+//-- --------------------------------------------
+
+
+console.log("");
+console.log('Temporizadores')
+
+//-- Ejemplo de uso de un temporizador
+
+//-- Función a ejecutar tras un tiempo
+//-- Función de retrollamada del temporizador
+function tarea1() {
+    console.log("Tarea 1 completada!");
+}
+
+
+//-- Llamada retardada mediante temporizador
+//-- Cuando transcurran 1000 ms se llama a la función tarea 1
+setTimeout(tarea1, 1000);
+
+//-- Esta estructura también es muy típica: incluir la función 
+//-- de retrollamada directamente en el parémtro, en vez de definirla
+//-- fuera
+setTimeout( () => {
+    console.log("Tarea 2 completada!");
+}, 2000);
+
+console.log("Esperando a que terminen las tareas");
+
+//-- Esta función de retrollamada se invoca cada 200ms
+//-- Se guarda su identificador en la variable id par
+//-- poder quitar el temporizador con ClearInterval 
+let id = setInterval( () => {
+    console.log("Tic...");
+}, 200 );
+
+//-- Al cabo de 3 segundos se desactiva el temporizador
+setTimeout( ()=> {
+  clearInterval(id)
+  console.log("Stop!");
+}, 3000);
