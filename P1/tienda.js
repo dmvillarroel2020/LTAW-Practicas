1bba99f404f53c8a8a2f822ea49ae3ba0d80a16d
dmvillarroel2020 <dm.villarroel.2020@alumnos.urjc.es>
2024-02-20 17:25:14 +0100

    tienda v1

--- P1/tienda.js
+++ P1/tienda.js
@@ -2,8 +2,8 @@ const http = require('http');
 
 const PUERTO = 9090;
 
-//-- Texto HTML
-const pagina = `
+//-- Texto HTML de la página principal
+const pagina_main = `
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,28 +11,77 @@ const pagina = `
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>¡Happy Server!</title>
+    <link rel="stylesheet" href="micss.css">
+    <title>Tienda</title>
 </head>
-<body style="background-color: lightblue">
-    <h1 style="color: green">HAPPY SERVER!!!</h1>
+<body>
+    <h1>MI TIENDA2</h1>
+    <p>Bienvenido a mi tienda</p>
+    <ul>
+        <li>
+            <a href="prod1.html">Producto 1</a>  
+        </li>
+        <li>
+            <a href="prod2.html">Producto 2</a>
+        </li>
+        <li>
+            <a href="prod3.html">Producto 3</a>
+        </li>
+
+        <img src="logo-urjc.png" alt="LOGO-URJC">
+
+    </ul>
+
 </body>
 </html>
 `
 
-const server = http.createServer((req, res) => {
+//-- Texto HTML de la página de error
+const pagina_error = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi tienda</title>
+</head>
+<body style="background-color: red">
+    <h1 style="color: white">ERROR!!!!</h1>
+</body>
+</html>
+`
 
-    console.log("Petición recibida")
-  
-    //-- Generar respuesta
-    //-- Código: Error. No encontrado
-    res.statusCode = 404;
-    res.statusMessage = "Not Found :-(";
-    res.setHeader('Content-Type', 'text/plain');
-    res.write("Soy el ANGRY Server\n");
-    res.end()
-  
-  });
+const server = http.createServer((req, res)=>{
+    console.log("Petición recibida!");
+
+    //-- Valores de la respuesta por defecto
+    let code = 200;
+    let code_msg = "OK";
+    let page = pagina_main;
+
+    //-- Analizar el recurso
+    //-- Construir el objeto url con la url de la solicitud
+    const url = new URL(req.url, 'http://' + req.headers['host']);
+    console.log(url.pathname);
+
+    //-- Cualquier recurso que no sea la página principal
+    //-- genera un error
+    if (url.pathname != '/') {
+        code = 404;
+        code_msg = "Not Found";
+        page = pagina_error;
+    }
+
+    //-- Generar la respusta en función de las variables
+    //-- code, code_msg y page
+    res.statusCode = code;
+    res.statusMessage = code_msg;
+    res.setHeader('Content-Type','text/html');
+    res.write(page);
+    res.end();
+});
 
 server.listen(PUERTO);
 
-console.log("SERVIDOR TIENDA DE CAMISETAS DE FÚTBOL. Escuchando en puerto: " + PUERTO);
+console.log("SERVER TIENDA DE CAMISETAS DE FÚTBOL: " + PUERTO);
