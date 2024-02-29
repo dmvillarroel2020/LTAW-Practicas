1110605b2bd39caab65fde4765b52ed1fd829d3a
dmvillarroel2020 <dm.villarroel.2020@alumnos.urjc.es>
2024-02-29 18:10:00 +0100

    no se muestran las imagenes en html

--- P1/tienda.js
+++ P1/tienda.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const PUERTO = 7070;
 
-// Rutas de los archivos HTML y CSS
+// Rutas de los archivos HTML, CSS y JS
 const pagina_main_path = 'index.html';
 const pagina_error_path = 'error.html';
 const prod1_path = 'prod1.html';
@@ -12,8 +12,14 @@ const prod2_path = 'prod2.html';
 const prod3_path = 'prod3.html';
 const estilo_css_path = 'estilo.css';
 
-// Ruta del directorio donde se encuentran las imágenes
-const directorio_imagenes = 'imagenes';
+// Tipos de contenido
+const tiposContenido = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
+    '.png': 'image/png',
+};
 
 // Función para leer archivos
 async function leerArchivo(path) {
@@ -43,7 +49,6 @@ async function servirArchivo(req, res, filePath, contentType) {
 const server = http.createServer(async (req, res) => {
     console.log("Petición recibida:", req.url);
 
-    // Obtener la ruta del archivo solicitado
     let filePath;
     switch (req.url) {
         case '/':
@@ -62,40 +67,15 @@ const server = http.createServer(async (req, res) => {
             filePath = estilo_css_path;
             break;
         default:
-            // Verificar si se solicita una imagen
-            if (req.url.startsWith(`/${directorio_imagenes}/`)) {
-                filePath = path.join(__dirname, req.url);
-            } else {
-                // Si la ruta no coincide con ninguna de las anteriores, se asume un recurso no encontrado
-                filePath = pagina_error_path;
-            }
+            // Verificar si se solicita un archivo de imagen
+            filePath = path.join(__dirname, req.url);
     }
 
-    // Determinar el tipo de contenido en función de la extensión del archivo
     const extname = path.extname(filePath);
-    let contentType = 'text/html';
-    switch (extname) {
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpeg';
-            break;
-    }
+    const contentType = tiposContenido[extname] || 'application/octet-stream';
 
     // Servir el archivo correspondiente
-    if (contentType.startsWith('text') || contentType === 'application/javascript') {
-        // Archivos de texto o JavaScript
-        const contenido = await leerArchivo(filePath);
-        res.writeHead(200, { 'Content-Type': contentType });
-        res.end(contenido);
-    } else {
-        // Archivos binarios (imágenes)
-        servirArchivo(req, res, filePath, contentType);
-    }
+    servirArchivo(req, res, filePath, contentType);
 });
 
 server.listen(PUERTO, () => {
