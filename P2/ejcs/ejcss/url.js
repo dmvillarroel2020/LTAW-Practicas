8a352728407d82f89325a08ebf80c36891746f83
dmvillarroel2020 <dm.villarroel.2020@alumnos.urjc.es>
2024-03-28 15:49:36 +0100

    cambio directorios

--- P2/ejcs/ejcss/url.js
+++
@@ -1,5 +0,0 @@
-//-- Construir un objeto URL
-const myURL = new URL('https://sub.example.com:8080/p/a/t/h?query1=string1&query2=string2#hash');
-
-//-- Imprimir el objeto URL para ver todas sus partes
-console.log(myURL); 
